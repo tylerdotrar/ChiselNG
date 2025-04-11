e72e6907ae1c4c966fce2fd8975393c7595d003c
unknown <tyler@hotbox.zip>
2025-04-11 09:16:55 -0500

    Adding modified chisel repo.

---
+++ go/chisel/share/version.go
@@ -0,0 +1,11 @@
+package chshare
+
+//ProtocolVersion of chisel. When backwards
+//incompatible changes are made, this will
+//be incremented to signify a protocol
+//mismatch.
+var ProtocolVersion = "chisel-v3"
+
+// Chisel repo clone date: 08 April 2025
+//var BuildVersion = "0.0.0-src-NG"
+var BuildVersion = "1.10.1-NG"
